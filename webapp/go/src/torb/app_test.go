1d44f3cb45c2aed87739eba388d49754854ab9c6
carimatics <github.carimatics@gmail.com>
2018-10-08 18:13:47 +0900

    initialize db for test

--- webapp/go/src/torb/app_test.go
+++ webapp/go/src/torb/app_test.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"os"
 	"src/github.com/joho/godotenv"
+	"src/github.com/labstack/gommon/log"
 	"testing"
 )
 
 func TestGetSheets(t *testing.T) {
 	godotenv.Load("../.env.sh")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8mb4",
+		os.Getenv("DB_USER"), os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"),
+		os.Getenv("DB_DATABASE"),
+	)
+
+	var err error
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	result, err := getSheets()
 	if err != nil {
