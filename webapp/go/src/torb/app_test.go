27c71c380002b4b29890685f012a31c900727f9a
carimatics <github.carimatics@gmail.com>
2018-10-08 18:11:41 +0900

    add godotenv

--- webapp/go/src/torb/app_test.go
+++ webapp/go/src/torb/app_test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"src/github.com/joho/godotenv"
 	"testing"
 )
 
 func TestGetSheets(t *testing.T) {
+	godotenv.Load("../.env.sh")
+
 	result, err := getSheets()
 	if err != nil {
 		t.Fatalf("failed test %#v", err)
